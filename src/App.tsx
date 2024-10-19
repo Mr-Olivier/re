5f75bceac2c63719b07e5ccb301137860d47a98b
Olivier <iradukundaolivier2000@gmail.com>
2024-10-19 20:59:57 +0200

    feat: Updating nested Object

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 94111,
+    },
   });
 
   const handleClick = () => {
-    setDrink({ ...drink, price: 6 });
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 94112 },
+    });
   };
 
-  return (
-    <div>
-      {drink.price}
-      <button onClick={handleClick}>Click Me</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
+
+// import { useState } from "react";
+
+// function App() {
+//   const [drink, setDrink] = useState({
+//     title: "Americano",
+//     price: 5,
+//   });
+
+//   const handleClick = () => {
+//     setDrink({ ...drink, price: 6 });
+//   };
+
+//   return (
+//     <div>
+//       {drink.price}
+//       <button onClick={handleClick}>Click Me</button>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // ort React from "react";
 // import Message from "./Message";
