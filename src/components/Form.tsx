61b61d7300f8e57e0c8a40d0346e1f26484e1ea5
Olivier <iradukundaolivier2000@gmail.com>
2024-10-22 13:18:19 +0200

    Disabling the Submit Button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////
-// Schema based Validation with Zod
+// Disabling the Submit Button
 
 import { useForm } from "react-hook-form";
 
@@ -19,7 +19,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FormData) => console.log(data);
@@ -50,7 +50,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
@@ -58,6 +58,67 @@ const Form = () => {
 };
 
 export default Form;
+
+////////////////////////////////////////////////////////
+// Schema based Validation with Zod
+
+// import { useForm } from "react-hook-form";
+
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+
+// const schema = z.object({
+//   name: z.string().min(3, { message: "Name must be at least 3 character" }),
+//   age: z
+//     .number({ invalid_type_error: "Age field is required." })
+//     .min(18, { message: "Age must be at least 18." }),
+// });
+
+// type FormData = z.infer<typeof schema>;
+
+// const Form = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+//   const onSubmit = (data: FormData) => console.log(data);
+
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-label">
+//           Name
+//         </label>
+//         <input
+//           {...register("name")}
+//           id="name"
+//           type="text"
+//           className="form-control"
+//         />
+//         {errors.name && <p className="text-danger">{errors.name.message}</p>}
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="age" className="form-label">
+//           Age
+//         </label>
+//         <input
+//           {...register("age", { valueAsNumber: true })}
+//           id="age"
+//           type="number"
+//           className="form-control"
+//         />
+//         {errors.age && <p className="text-danger">{errors.age.message}</p>}
+//       </div>
+//       <button className="btn btn-primary" type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default Form;
 
 ////////////////////////////////////////////////////////
 // Applying Validation
