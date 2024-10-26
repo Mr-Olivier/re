34f525aaceec91815f369650d428841a125497d2
Olivier <iradukundaolivier2000@gmail.com>
2024-10-26 11:13:30 +0200

    feat: Creating a custom Data fetching Hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,44 +4,10 @@
 import React, { useEffect, useState } from "react";
 import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     try {
-  //       const res = await axios.get<User[]>(
-  //         "https://jsonplaceholder.typicode.com/users"
-  //       );
-  //       setUsers(res.data);
-  //     } catch (err) {
-  //       setError((err as AxiosError).message);
-  //     }
-  //   };
-
-  //   fetchUsers();
-  // }, []);
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
